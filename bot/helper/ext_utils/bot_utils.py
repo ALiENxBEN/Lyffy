5cc69ce7329c694166e4075e287f5d3fbc9ad559
Anuragben10 <112600905+Anuragben10@users.noreply.github.com>
2023-09-02 12:32:51 +0530

    bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -157,7 +157,7 @@ def source(self):
     return source
 
 def get_readable_message():
-    msg = '<b>Powered by __《 ⋆⋆  🎀 𝔹𝔼ℕ 𝕩 𝕃𝔼𝔼ℂℍ 🎀  ⋆⋆ 》__</b>\n\n'
+    msg = '<b>Powered by 😎 @Moviezpure 😎</b>\n\n'
     button = None
     tasks = len(download_dict)
     currentTime = get_readable_time(time() - botStartTime)
